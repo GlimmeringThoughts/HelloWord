55a6059844740e210bf6802e87c4cd9eb977b6a1
weida lin <>
2024-07-02 21:28:30 +0800

    initial project

---
+++ XCodeAIExtension/XCodeAIExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeAIExtensionApp.swift
+//  XCodeAIExtension
+//
+//  Created by weida lin on 2024/7/2.
+//
+
+import SwiftUI
+
+@main
+struct XCodeAIExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
